6c93b8f3bd29509489de599a4fdd8d47e7b373ca
YaqubRahman <Yaqub.Rahman@city.ac.uk>
2025-02-24 19:56:24 +0000

    feat/encapsulated-everything-and-used-inheritance-and-added-background

--- src/game/GameView.java
+++ src/game/GameView.java
@@ -1,8 +1,19 @@
 package game;
 import city.cs.engine.UserView;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GameView extends UserView {
+    private Image background;
+
     public GameView(GameWorld world, int width, int height) {
         super(world, width, height);
+        background = new ImageIcon("data/Background.jpg").getImage();
+    }
+
+    @Override
+    protected void paintBackground(Graphics2D g){
+        g.drawImage(background, 0, 0, this);
     }
 }
