80a02a5fafee0df70010891dc85d6238b255d14b
YaqubRahman <Yaqub.Rahman@city.ac.uk>
2025-03-17 11:20:58 +0000

    feat: coin pickip sound

--- src/game/Coin.java
+++ src/game/Coin.java
@@ -2,21 +2,39 @@ package game;
 
 import city.cs.engine.*;
 import org.jbox2d.common.Vec2;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class Coin extends Walker {
     private static final Shape coinShape = new CircleShape(0.7F);
+    private static SoundClip coinPickupSound;
     private static BodyImage image = new BodyImage("data/Coin.gif");
 
-    private boolean isPickedUp;
+    static{
+        try{
+            coinPickupSound = new SoundClip("data/CoinPickupSound.wav");
+            System.out.println("Loading coin pickup sound");
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.out.println(e);
+        }
+    }
 
     public Coin(World world){
         super(world, coinShape);
         addImage(image);
+    }
+
+    @Override
+    public void destroy(){
+        coinPickupSound.play();
+        super.destroy();
     }
 }
